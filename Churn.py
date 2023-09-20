e3ac0a8943803ce92359fe994bb4eac8ea4c0bb3
spackle-xmr <93713512+spackle-xmr@users.noreply.github.com>
2023-09-20 18:21:04 -0500

    Update Churn.py
    Remove subaddress send option

--- Churn.py
+++ Churn.py
@@ -24,8 +24,6 @@ rpc_user,rpc_password = 'USER', 'PASS'
 rpc_wallet_connection = AuthServiceProxy('http://{0}:{1}@127.0.0.1:16969/json_rpc'.format(rpc_user, rpc_password)) 
 cells = [] #List of all currently running timers/transactions
 main_address = rpc_wallet_connection.get_address()['addresses'][0]['address'] #Get main address
-creation=rpc_wallet_connection.create_address({"account_index":0,"count":1}) # Create 1 subaddress
-subaddress = rpc_wallet_connection.get_address()['addresses'][1]['address'] #Get subaddress
 height = rpc_wallet_connection.get_height()['height'] #Get height
 print("Churning wallet")
 
@@ -43,11 +41,7 @@ while 1:
         for i in cells: #Check cells
             if (i.timer <= currentHeight): #If it is time, send transaction
                 print("Churning key image :",i.key_image)
-                address_select = np.random.randint(1,5) # Randomly send to subaddress 25% of the time
-                if address_select == 1: # Send to subaddress
-                    sweep_data = rpc_wallet_connection.sweep_single({'address':subaddress,'key_image':i.key_image,'outputs':1,'do_not_relay':False})
-                else: # Send to main address
-                    sweep_data = rpc_wallet_connection.sweep_single({'address':main_address,'key_image':i.key_image,'outputs':1,'do_not_relay':False})
+                sweep_data = rpc_wallet_connection.sweep_single({'address':main_address,'key_image':i.key_image,'outputs':1,'do_not_relay':False})
                 cells.remove(i) #Remove spent key image entry
                 print("Cells :") #Display active cells
                 for item in cells: print(vars(item))
